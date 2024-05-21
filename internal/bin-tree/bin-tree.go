4b4737076d0f28e44e537cfb7c8d187d902c139c
AmiciaDeMonfourt <cvetochek0@list.ru>
2024-05-21 03:12:38 +0300

    binary tree: insert, delete ready

--- internal/bin-tree/bin-tree.go
+++ internal/bin-tree/bin-tree.go
@@ -2,18 +2,21 @@ package bintree
 
 import "fmt"
 
-type binaryTree struct {
+type BinaryTree struct {
 	root   *node
 	height int
 }
 
-func New() *binaryTree {
-	return &binaryTree{
+func New() *BinaryTree {
+	return &BinaryTree{
 		root: nil,
 	}
 }
 
-func (t *binaryTree) Insert(data interface{}, key int) {
+// Insert new item in binary tree
+// Insert into an empty place, that finded by rule:
+// if new node key > current subtree root key -> descent to the right. Else descent to the left
+func (t *BinaryTree) Insert(data interface{}, key int) {
 	newNode := newNode(data, key)
 	cursor := t.root
 	height := 0
@@ -23,41 +26,34 @@ func (t *binaryTree) Insert(data interface{}, key int) {
 		return
 
 	} else {
-
 		for {
 			height++
+			// calculate binary tree height
 			if height > t.height {
 				t.height = height
 			}
 
 			if key > cursor.Key {
+				// if right element is free - add new element
 				if cursor.Right == nil {
 					cursor.Right = newNode
 					return
-
-				} else if key > cursor.Right.Key {
-					newNode.Left = cursor.Right
-					cursor.Right = newNode
-					return
 				}
-
+				// else descent down
 				cursor = cursor.Right
 				continue
 
 			} else if key < cursor.Key {
+				// is there left element is free - add new element on this place
 				if cursor.Left == nil {
 					cursor.Left = newNode
 					return
-
-				} else if key > cursor.Left.Key {
-					newNode.Left = cursor.Left
-					cursor.Left = newNode
-					return
 				}
-
+				// else descent down
 				cursor = cursor.Left
 				continue
 
+				// if tree has a node with equal key
 			} else {
 				cursor.Data = data
 				return
@@ -66,23 +62,97 @@ func (t *binaryTree) Insert(data interface{}, key int) {
 	}
 }
 
-// Remove item from tree. Two cases:
-//   if deleting item has no right child element
-//		 replace the deleted item with the left child
-//	 else
-//	     find the minimum element in right subtree and replace it with the deleted element
-func (t *binaryTree) Delete(key int) {
+// Remove item from tree. Four cases by element to be deleted childs:
+// 1)left and right nil
+// 2)left nil
+// 3)right nil
+// 4)left and right child not nil
+func (t *BinaryTree) Delete(key int) {
 	if t.root == nil {
 		panic("tree is empty, nothing to delete")
 	}
 
+	rmNode := t.findNodeByKey(key)
+	rmNodeParent := t.findParentByKey(key)
+
+	// Case where record to be deleted has only the left child.
+	// Just replace the right node and the node to be deleted
+	if rmNode.Left == nil && rmNode.Right == nil {
+		if rmNodeParent.Left == rmNode {
+			rmNodeParent.Left = nil
+		} else {
+			rmNodeParent.Right = nil
+		}
+
+		// Case where record to be deleted has only the left child.
+		// Just replace the right node and the node to be deleted
+	} else if rmNode.Right == nil {
+		if rmNodeParent.Left == rmNode {
+			rmNodeParent.Left = rmNode.Left
+		} else {
+			rmNodeParent.Right = rmNode.Left
+		}
+		return
+
+		// Case where record to be deleted has only the right child
+		// 1)find minimal element and his parent in the right subtree,
+		// 2)replace min and the record to be deleted
+		// 3)update the minimal element and the record to be deleted parent's pointers
+	} else if rmNode.Left == nil {
+		min, minParent := t.findMinumum(rmNode.Right)
+		minParent.Left = nil
+
+		if rmNodeParent.Left == rmNode {
+			rmNodeParent.Left = min
+		} else {
+			rmNodeParent.Right = min
+		}
+		return
+
+		// Case where: record to be deleted has only the right child
+		// 1)find minimal element and his parent in the right subtree
+		// 2)replace min and the record to be deleted
+		// 3)update the minimal element and the record to be deleted parent's pointers
+	} else {
+		min, minParent := t.findMinumum(rmNode.Right)
+		minParent.Left = nil
+
+		if rmNodeParent.Left == rmNode {
+			rmNodeParent.Left = min
+		} else {
+			rmNodeParent.Right = min
+		}
+
+		// update pointers at the new subtree root
+		min.Left = rmNode.Left
+		if rmNode.Right != min {
+			min.Right = rmNode.Right
+		}
+
+		return
+	}
+
 }
 
-func (t *binaryTree) Search(key int) interface{} {
+func (t *BinaryTree) findMinumum(subtreeRoot *node) (min *node, hisParent *node) {
+	if subtreeRoot == nil {
+		return nil, nil
+	}
+
+	parent := subtreeRoot
+	for subtreeRoot.Left != nil {
+		parent = subtreeRoot
+		subtreeRoot = subtreeRoot.Left
+	}
+
+	return subtreeRoot, parent
+}
+
+func (t *BinaryTree) Search(key int) interface{} {
 	return t.findNodeByKey(key).Data
 }
 
-func (t *binaryTree) findParentByKey(key int) *node {
+func (t *BinaryTree) findParentByKey(key int) *node {
 	child := t.root
 	parent := t.root
 
@@ -105,7 +175,7 @@ func (t *binaryTree) findParentByKey(key int) *node {
 	panic(fmt.Sprintf("There is no record with this key: %d", key))
 }
 
-func (t *binaryTree) findNodeByKey(key int) *node {
+func (t *BinaryTree) findNodeByKey(key int) *node {
 	cursor := t.root
 
 	for cursor != nil {
@@ -126,17 +196,24 @@ func (t *binaryTree) findNodeByKey(key int) *node {
 }
 
 // pre order traverse, using recursive descent
-func (t *binaryTree) TraverseTree() {
+func (t *BinaryTree) TraverseTree() []interface{} {
 	var descent func(*node)
+	keys := make([]interface{}, 0)
 
 	descent = func(cursor *node) {
 		if cursor == nil {
 			return
 		}
-		fmt.Printf("%d ", cursor.Key)
+		keys = append(keys, cursor.Key)
+
 		descent(cursor.Left)
 		descent(cursor.Right)
 	}
 
 	descent(t.root)
+	return keys
+}
+
+func (t *BinaryTree) GetHeight() int {
+	return t.height
 }
--- internal/bin-tree/bin-tree_test.go
+++ internal/bin-tree/bin-tree_test.go
@@ -1,14 +1,13 @@
-package bintree_test
+package bintree
 
 import (
-	bintree "data-structures/internal/bin-tree"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestBinaryTree_Insert(t *testing.T) {
-	tree := bintree.New()
+	tree := New()
 
 	tree.Insert("data1", 6)
 	tree.Insert("data2", 8)
@@ -19,11 +18,15 @@ func TestBinaryTree_Insert(t *testing.T) {
 	tree.Insert("data4", -1)
 	tree.Insert("data4", 18)
 
-	tree.TraverseTree()
+	assert.Equal(
+		t,
+		[]interface{}{6, 1, 0, -1, 2, 8, 9, 18},
+		tree.TraverseTree(),
+	)
 }
 
 func TestBinaryTree_Search(t *testing.T) {
-	tree := bintree.New()
+	tree := New()
 
 	assert.Panics(t, func() {
 		tree.Search(0)
@@ -45,13 +48,54 @@ func TestBinaryTree_Search(t *testing.T) {
 }
 
 func TestBinaryTree_Delete(t *testing.T) {
-	tree := bintree.New()
+	tree := getTestBinaryTree()
+
+	// delete node without childs
+	tree.Delete(7)
+	assert.Equal(t, []any{6, 1, 10, 8, 9, 11}, tree.TraverseTree())
+
+	// deleete node with only right child
+	tree.Delete(10)
+	assert.Equal(t, []any{6, 1, 11, 8, 9}, tree.TraverseTree())
+
+	// delete node with 2 childs
+	tree.Delete(8)
+	assert.Equal(t, []any{6, 1, 11, 9}, tree.TraverseTree())
+
+	// delete node with only left child
+	tree.Delete(11)
+	assert.Equal(t, []any{6, 1, 9}, tree.TraverseTree())
+}
+
+func TestBinaryTree_FindMinimum(t *testing.T) {
+	tree := getTestBinaryTree()
+
+	// minimal in tree
+	min, minParent := tree.findMinumum(tree.findNodeByKey(6))
+	assert.Equal(t, 1, min.Key)
+	assert.Equal(t, 6, minParent.Key)
+
+	// minimal in right root subtree
+	min, minParent = tree.findMinumum(tree.findNodeByKey(10))
+	assert.Equal(t, 7, min.Key)
+	assert.Equal(t, 8, minParent.Key)
+
+	// minimal in subtree, but substree has one node
+	min, minParent = tree.findMinumum(tree.findNodeByKey(11))
+	assert.Equal(t, 11, min.Key)
+	assert.Equal(t, 11, minParent.Key)
+}
+
+func getTestBinaryTree() *BinaryTree {
+	tree := New()
 
 	tree.Insert("6", 6)
-	tree.Insert("8", 8)
 	tree.Insert("1", 1)
+	tree.Insert("10", 10)
+	tree.Insert("8", 8)
+	tree.Insert("7", 7)
 	tree.Insert("9", 9)
+	tree.Insert("11", 11)
 
-	tree.Delete(6)
-	tree.TraverseTree()
+	return tree
 }
